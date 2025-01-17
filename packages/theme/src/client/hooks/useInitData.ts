09078732df3771e02ab00ef13df280958e681714
hacxy <hacxy.js@outlook.com>
2025-01-17 18:21:16 +0800

    fix: adjust the call timing of useBaseStore

--- packages/theme/src/client/hooks/useInitData.ts
+++ packages/theme/src/client/hooks/useInitData.ts
@@ -1,14 +1,15 @@
 import type { NProgress } from 'nprogress';
-import { inject } from 'vue';
+import { inject, onMounted } from 'vue';
 import { data } from '../datas/base.data';
 import { useBaseStore } from '../stores/base';
 
 export function useInitData() {
   const np = inject<NProgress>('progress');
-  const baseStore = useBaseStore();
-  baseStore.autoSidebar = data.autoSidebar;
-  baseStore.articleList = data.list;
-
+  onMounted(() => {
+    const baseStore = useBaseStore();
+    baseStore.autoSidebar = data.autoSidebar;
+    baseStore.articleList = data.list;
+  });
   return {
     np
   };
