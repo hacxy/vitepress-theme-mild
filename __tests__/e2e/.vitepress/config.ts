b8e0f4958d6ebc2ead59d05875c328bc9e2b0c21
hacxy <hacxy.js@outlook.com>
2025-08-11 11:22:45 +0800

    fix(theme): Compatible with vitepress-plugin-pagefind

--- __tests__/e2e/.vitepress/config.ts
+++ __tests__/e2e/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import type { ThemeConfig } from 'vitepress-theme-mild';
 import { defineConfigWithTheme } from 'vitepress';
+import { pagefindPlugin } from 'vitepress-plugin-pagefind';
 import baseConfig from 'vitepress-theme-mild/config';
 
 export default defineConfigWithTheme<ThemeConfig>({
@@ -21,5 +22,10 @@ export default defineConfigWithTheme<ThemeConfig>({
     project: {
       list: [{ title: '2048-cli-game', description: '2048 game for terminal.', repoUrl: 'https://github.com/hacxy/2048-cli-game', stars: 1, tags: [], forks: 0, language: 'TypeScript', lastUpdated: '2025-07-15 21:39:12' }, { title: 'cli-unbuild-template', description: 'CLI application development template using unbuild as a build tool.', repoUrl: 'https://github.com/hacxy/cli-unbuild-template', stars: 4, tags: ['cli', 'cli-template', 'nodejs', 'ts-cli-template', 'typescript'], forks: 0, language: 'JavaScript', lastUpdated: '2025-03-31 12:35:59' }, { title: 'create-hacxy-app', description: 'This is a demo of a nodejs scaffolding project.', repoUrl: 'https://github.com/hacxy/create-hacxy-app', stars: 1, tags: [], forks: 1, language: 'JavaScript', lastUpdated: '2025-04-22 15:39:39' }, { title: 'create-ts-frame', description: 'This is a scaffolding tool used to quickly create starter project templates with Typescript as the main development language. Templates include but are not limited to web applications, mini programs, CLI command line tools, and more.', repoUrl: 'https://github.com/hacxy/create-ts-frame', stars: 4, tags: ['cli', 'create', 'create-typescript-project', 'typescript-cli', 'typescript-template'], forks: 1, language: 'TypeScript', lastUpdated: '2024-12-16 13:50:51' }, { title: 'json2ts', description: 'Json to typescript types', repoUrl: 'https://github.com/hacxy/json2ts', stars: 1, tags: [], forks: 0, language: 'TypeScript', lastUpdated: '2024-11-01 17:28:17' }, { title: 'json2typebox', description: 'Creating TypeBox code from Json Data', repoUrl: 'https://github.com/hacxy/json2typebox', stars: 6, tags: ['json2typebox', 'typebox', 'typescript'], forks: 0, language: 'TypeScript', lastUpdated: '2024-11-05 00:34:18' }, { title: 'json2typebox-cli', description: 'Convert JSON data into TypeBox types using a command-line tool.', repoUrl: 'https://github.com/hacxy/json2typebox-cli', stars: 4, tags: ['cli', 'json2typebox', 'typebox', 'typescript'], forks: 0, language: 'JavaScript', lastUpdated: '2024-11-06 18:06:33' }, { title: 'l2d', description: '在浏览器中加载Live2D模型更简单', repoUrl: 'https://github.com/hacxy/l2d', stars: 20, tags: ['live2d', 'live2d-web'], forks: 2, language: 'TypeScript', lastUpdated: '2025-07-16 17:29:31' }]
     }
+  },
+  vite: {
+    plugins: [
+      pagefindPlugin()
+    ]
   }
 });
--- src/client/components/NavBar.vue
+++ src/client/components/NavBar.vue
@@ -5,12 +5,12 @@ import VPNavBarAppearance from 'vitepress/dist/client/theme-default/components/V
 import VPNavBarExtra from 'vitepress/dist/client/theme-default/components/VPNavBarExtra.vue';
 import VPNavBarHamburger from 'vitepress/dist/client/theme-default/components/VPNavBarHamburger.vue';
 import VPNavBarMenu from 'vitepress/dist/client/theme-default/components/VPNavBarMenu.vue';
-import VPNavBarSearch from 'vitepress/dist/client/theme-default/components/VPNavBarSearch.vue';
 import VPNavBarSocialLinks from 'vitepress/dist/client/theme-default/components/VPNavBarSocialLinks.vue';
 import VPNavBarTranslations from 'vitepress/dist/client/theme-default/components/VPNavBarTranslations.vue';
 import { ref, watchPostEffect } from 'vue';
 import { useSidebar } from '../hooks/useSidebar';
 import VPNavBarTitle from './NavBarTitle.vue';
+import VPNavBarSearch from './VPNavBarSearch.vue';
 
 const props = defineProps<{
   isScreenOpen: boolean
@@ -31,7 +31,7 @@ watchPostEffect(() => {
     'has-sidebar': hasSidebar.value,
     'home': frontmatter.value.layout === 'home',
     'top': y.value === 0,
-    'screen-open': props.isScreenOpen
+    'screen-open': props.isScreenOpen,
   };
 });
 </script>
@@ -127,8 +127,8 @@ watchPostEffect(() => {
   pointer-events: none;
 }
 
-.container > .title,
-.container > .content {
+.container>.title,
+.container>.content {
   pointer-events: none;
 }
 
@@ -213,11 +213,11 @@ watchPostEffect(() => {
   }
 }
 
-.menu + .translations::before,
-.menu + .appearance::before,
-.menu + .social-links::before,
-.translations + .appearance::before,
-.appearance + .social-links::before {
+.menu+.translations::before,
+.menu+.appearance::before,
+.menu+.social-links::before,
+.translations+.appearance::before,
+.appearance+.social-links::before {
   margin-right: 8px;
   margin-left: 8px;
   width: 1px;
@@ -226,12 +226,12 @@ watchPostEffect(() => {
   content: '';
 }
 
-.menu + .appearance::before,
-.translations + .appearance::before {
+.menu+.appearance::before,
+.translations+.appearance::before {
   margin-right: 16px;
 }
 
-.appearance + .social-links::before {
+.appearance+.social-links::before {
   margin-left: 16px;
 }
 
